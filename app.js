9205b4e16ed7c84ba6d93bf0395c537df653431a
Brooks Patton <brooks.patton@galvanize.com>
2016-08-24 10:41:23 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+let app = express();
+
+app.get('/', (req, res) => {
+  res.json({message: 'Hello from express!'});
+});
+
+app.get('/page2', (req, res) => {
+  res.json({message: 'I am page 2!!!!'});
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
